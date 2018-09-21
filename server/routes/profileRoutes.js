0ad85224a6f8441afddd80f7795a5314bccbd833
Manuel <m.a.c.p90@gmail.com>
2018-09-21 12:54:15 -0400

    other routes set up

--- server/routes/profileRoutes.js
+++ server/routes/profileRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
+
 // @route  GET ‘/profile'
 // @desct  display user information
 // @access.  private
