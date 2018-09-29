615b26334deba269ee73a3eb950958c46157f0d3
Manuel <m.a.c.p90@gmail.com>
2018-09-29 09:35:54 -0400

    notes routes done

--- server/routes/api/notesRoutes.js
+++ server/routes/api/notesRoutes.js
@@ -1,5 +1,26 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../../models/user");
+const Job = require("../../models/job");
 
+router.post("/note/add", (req, res, next) => {
+  const theUser = req.user;
+  if (theUser._id) {
+    Note.create({ content: req.body.content })
+      .then(response => {
+        Job.findByIdAndUpdate(req.body._id, { $push: { notes: response } })
+          .then(response => {
+            console.log("user update -=-=", theUser);
+            res.json(response);
+          })
+          .catch(() => {
+            res.json(err);
+          });
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  } else {
+    res.json("login");
+  }
+});
 module.exports = router;
